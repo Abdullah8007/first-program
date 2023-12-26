b867380b3043ab46d01bfd3c10aadb877094adfb
Abdullah8007 <abdullahharishjagrala@gmail.com>
2023-12-26 23:40:33 +0530

    ADD: 122333

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,17 @@
 /* eslint-disable react/jsx-no-undef */
 import React from 'react'
+import styled from 'styled-components';
 
 function Home() {
   return (
-    <Hero>
+    <Container>
       <h1 className="title">Welcome to the Tailwind CSS Playground</h1>
-    </Hero>
+    </Container>
   )
-}
+};
 
-export default Home
+const Container = styled.div`
+  text-align: center;
+`;
+
+export default Home;
