2225237d43c9c654efbaf4eb02bb2ce0d397e361
Abdullah8007 <abdullahharishjagrala@gmail.com>
2024-01-05 12:42:00 +0530

    ADD: 147258

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -10,15 +10,17 @@ function Projects() {
                 <Row className='d-flex align-items-center justify-content-evenly flex-wrap flex-sm-nowrap gap-3'>
                     <Column className='col-lg-5 col-md-6 col-sm-6'>
                         <Logo src="/Images/MovieMetropolis.png" alt="MovieMetropolis" className='w-100 h-auto' />
-                        <div className="layer">
-                            <h3 className='d-none'>MovieMetropolis</h3>
-                        </div>
+                        <Layer className="layer">
+                            <h3 className=''>MovieMetropolis</h3>
+                            <p>React JS</p>
+                        </Layer>
                     </Column>
                     <Column className='col-lg-5 col-md-6 col-sm-6'>
                         <Logo src="/Images/Eduford.png" alt="Eduford" className='w-100 h-auto' />
-                        <div className="layer">
-                            <h3 className='d-none'>Eduford</h3>
-                        </div>
+                        <Layer className="layer">
+                            <h3 className=''>Eduford</h3>
+                            <p>HTML,CSS,JS</p>
+                        </Layer>
                     </Column>
                 </Row>
             </SubContainer>
@@ -59,6 +61,13 @@ const Column = styled.div`
     box-shadow: 0 10px 10px #B0B6C0;
     border-radius: 10px;
     padding: 10px;
+`;
+
+const Layer = styled.div`
+        text-align: center;
+        transform: translateY(-75px);
+        display: none;
+        letter-spacing: 1px;
 `;
 
 const Logo = styled.img``;
