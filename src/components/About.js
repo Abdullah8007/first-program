32fc003331e4744d42b693787407b040431f49d9
Abdullah8007 <abdullahharishjagrala@gmail.com>
2024-01-08 13:27:52 +0530

    ADD: 147852

--- src/components/About.js
+++ src/components/About.js
@@ -1,97 +1,126 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
-import styled from 'styled-components';
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+import '../styles/AboutStyle.css';
+import { Container, Row, Col } from 'react-bootstrap';
 
 function About() {
   return (
-    <Container className='container-fluid' id='about'>
-      <SubContainer className='container'>
-        <Row className='row justify-content-between'>
-          <Image className='col-lg-4 img' src='/Images/about-img.png' alt='About-Img' />
-          <AboutMe className='col-lg-8'>
-            <Title>About</Title>
-            <Head className=' mb-5 mb-sm-1'>Who I Am</Head>
-            <Para>Hi there!, I’m Abdullah, a curious frontend developer who’s trying to get a bit better every day.
-              <br></br><br></br>
-              I currently studying in S.Y.BCA from Tilak Maharashtra Vidyapeeth, Pune.
-              <br></br><br></br>
-              I have always had a knack for technology and working with computers. Fascinated with how intricate programming can be I was quickly drawn to learn more. I started learning JavaScript and was even more enthused with making websites interactive.
-              <br></br><br></br>
-              I’m very dedicated to learn new things and to truly believe that you should never stop learning. I enjoy creating different things, whether that be websites, application or anything in between.
-              <br></br><br></br>
-              I’m always looking for new opportunities to expand my understanding of web design and development, so you’ll generally find me sipping a delicious cup of coffee with my nose in a good “technical” book or plugged into my laptop drooling over all of the beauty the web has to offer. I hope to join a team of talented individuals I can learn from and build engaging digital experiences with.
-              <br></br><br></br>
-              <a href=''>Check out some of my latest projects</a>
-            </Para>
-          </AboutMe>
-        </Row>
-      </SubContainer>
-    </Container>
+    // <Container className='container-fluid' id='about'>
+    //   <SubContainer className='container'>
+    //     <Row className='row justify-content-between'>
+    //       <Image className='Col-lg-4 img' src='/Images/about-img.png' alt='About-Img' />
+    //       <AboutMe className='Col-lg-8'>
+    //         <h4>About</h4>
+    //         <Head className=' mb-5 mb-sm-1'>Who I Am</Head>
+    // <Para>Hi there!, I’m Abdullah, a curious frontend developer who’s trying to get a bit better every day.
+    //   <br></br><br></br>
+    //   I currently studying in S.Y.BCA from Tilak Maharashtra Vidyapeeth, Pune.
+    //   <br></br><br></br>
+    //   I have always had a knack for technology and working with computers. Fascinated with how intricate programming can be I was quickly drawn to learn more. I started learning JavaScript and was even more enthused with making websites interactive.
+    //   <br></br><br></br>
+    //   I’m very dedicated to learn new things and to truly believe that you should never stop learning. I enjoy creating different things, whether that be websites, application or anything in between.
+    //   <br></br><br></br>
+    //   I’m always looking for new opportunities to expand my understanding of web design and development, so you’ll generally find me sipping a delicious cup of coffee with my nose in a good “technical” book or plugged into my laptop drooling over all of the beauty the web has to offer. I hope to join a team of talented individuals I can learn from and build engaging digital experiences with.
+    //   <br></br><br></br>
+    //   <a href=''>Check out some of my latest projects</a>
+    // </Para>
+    //       </AboutMe>
+    //     </Row>
+    //   </SubContainer>
+    // </Container>
+
+    <section className='about-section' id='about'>
+      <Container fluid>
+        <Container>
+          <Row className='justify-content-center align-items-center'>
+            <h3 className='head'>About</h3>
+            <h1 className='title'>Who I Am</h1>
+            <Col lg={5} className='mb-5 mb-lg-0'>
+              <img src='/Images/about-img.png' alt='About-Img' className='img-fluid' />
+            </Col>
+            <Col lg={7} className='mb-5 mb-lg-0'>
+              <p className='parah'>
+                Hi there!, I’m Abdullah, a curious frontend developer who’s trying to get a bit better every day.
+                <br></br><br></br>
+                I currently studying in S.Y.BCA from Tilak Maharashtra Vidyapeeth, Pune.
+                <br></br><br></br>
+                I have always had a knack for technology and working with computers. Fascinated with how intricate programming can be I was quickly drawn to learn more. I started learning JavaScript and was even more enthused with making websites interactive.
+                <br></br><br></br>
+                I’m very dedicated to learn new things and to truly believe that you should never stop learning. I enjoy creating different things, whether that be websites, application or anything in between.
+                <br></br><br></br>
+                I’m always looking for new opportunities to expand my understanding of web design and development, so you’ll generally find me sipping a delicious cup of coffee with my nose in a good “technical” book or plugged into my laptop drooling over all of the beauty the web has to offer. I hope to join a team of talented individuals I can learn from and build engaging digital experiences with.
+                <br></br><br></br>
+                <a href=''>Check out some of my latest projects</a>
+              </p>
+            </Col>
+          </Row>
+        </Container>
+      </Container>
+    </section>
+
   )
 };
 
-const Container = styled.div`
-    height: 100vh;
-    display: flex;
-    // justify-content: space-between;
-    align-items: center;
-    background-color: #ECF0F3;
+// const Container = styled.h4`
+//     height: 100vh;
+//     display: flex;
+//     // justify-content: space-between;
+//     align-items: center;
+//     background-color: #ECF0F3;
 
-    .img:hover {
-      transform: scale(1.05, 1.05);
-  }
-`;
+//     .img:hover {
+//       transform: scale(1.05, 1.05);
+//   }
+// `;
 
-const AboutMe = styled.div``;
+// const Col = styled.h4``;
 
-const Row = styled.div`
+// const Row = styled.h4``;
 
-`;
+// const SubContainer = styled.h4``;
 
-const SubContainer = styled.div``;
+// const h4 = styled.h4`
+// font-size: 20px;
+// text-transform: uppercase;
+// color: #6A994E;
+// letter-spacing: 1px;
+// `;
 
-const Title = styled.div`
-  font-size: 20px;
-  text-transform: uppercase;
-  color: #6A994E;
-  letter-spacing: 1px;
-`;
+// const Head = styled.h4`
+// font-size: 32px;
+// padding: 16px 0;
+// font-weight: 700;
+// letter-spacing: 1px;
+// `;
 
-const Head = styled.div`
-  font-size: 32px;
-  padding: 16px 0;
-  font-weight: 700;
-  letter-spacing: 1px;
-`;
+// const Para = styled.h4`
+//   font-size: 16px;
+//   line-height: 22px;
+//   letter-spacing: 1px;
 
-const Para = styled.div`
-  font-size: 16px;
-  line-height: 22px;
-  letter-spacing: 1px;
+//   a {
+//     color: #434343;
+//   }
+// `;
 
-  a {
-    color: #434343;
-  }
-`;
+// const Image = styled.img`
+//   // width: 500px;
+//   // height: auto;
+//   border: 10px solid #fff;
+//   border-radius: 10px;
+//   box-shadow: 0 10px 10px #B0B6C0;
+//   transition: .5s;
 
-const Image = styled.img`
-  // width: 500px;
-  // height: auto;
-  border: 10px solid #fff;
-  border-radius: 10px;
-  box-shadow: 0 10px 10px #B0B6C0;
-  transition: .5s;
+//   @media (max-width: 1024px) {
+//     // width: 400px;
+//     height: auto;
+//   }
 
-  @media (max-width: 1024px) {
-    // width: 400px;
-    height: auto;
-  }
-
-  @media (max-width: 768px) {
-    width: 300px;
-    height: auto;
-  }
-`;
+//   @media (max-width: 768px) {
+//     width: 300px;
+//     height: auto;
+//   }
+// `;
 
 export default About;
--- src/components/Header.js
+++ src/components/Header.js
@@ -5,6 +5,7 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import '../styles/HeaderStyle.css';
+import { Link } from 'react-scroll';
 
 
 const Header = () => {
@@ -32,12 +33,12 @@ const Header = () => {
               className="ms-auto my-2 my-lg-0 align-items-center text-uppercase"
               navbarScroll
             >
-              <Nav.Link Link to="home" spy={true} smooth={true} offset={50} duration={100} className='nav-menu'>Home</Nav.Link>
-              <Nav.Link Link to="about" spy={true} smooth={true} offset={50} duration={100} className='nav-menu'>About</Nav.Link>
-              <Nav.Link Link to="skills" spy={true} smooth={true} offset={50} duration={100} className='nav-menu'>Skills</Nav.Link>
-              <Nav.Link Link to="projects" spy={true} smooth={true} offset={50} duration={100} className='nav-menu'>Projects</Nav.Link>
-              <Nav.Link Link to="contact" spy={true} smooth={true} offset={50} duration={100} className='nav-menu'>Contact</Nav.Link>
-              <a href='/Resume.pdf' target='_blank' rel='noreferrer'><Button className='btn-main nav-menu'>Resume</Button></a>
+              <Nav.Link className='nav-menu'><Link to="home" spy={true} smooth={true} offset={0} duration={100}>Home</Link></Nav.Link>
+              <Nav.Link className='nav-menu'><Link to="about" spy={true} smooth={true} offset={0} duration={100}>About</Link></Nav.Link>
+              <Nav.Link className='nav-menu'><Link to="skills" spy={true} smooth={true} offset={0} duration={100}>Skills</Link></Nav.Link>
+              <Nav.Link className='nav-menu'><Link to="projects" spy={true} smooth={true} offset={0} duration={100}>Projects</Link></Nav.Link>
+              <Nav.Link className='nav-menu'><Link to="contact" spy={true} smooth={true} offset={0} duration={100}>Contact</Link></Nav.Link>
+              <a href='/Resume.pdf' target='_blank' rel='noreferrer'><Button className='btn-main'>Resume</Button></a>
             </Nav>
           </Navbar.Collapse>
         </Container>
@@ -47,109 +48,5 @@ const Header = () => {
 };
 
 const Button = styled.button``;
-
-// const Container = Styled.div``;
-
-// const SubContainer = Styled.div`
-//   display: flex;
-//   align-items: center;
-//   justify-content: space-between;
-// `;
-
-// const Nav = Styled.div`
-//   height: 80px;
-//   width: 100%;
-//   position: fixed;
-//   background-color: #ECF0F3;
-//   // box-shadow: 0 10px 10px #c9c9c9;
-//   z-index: 1;
-//   display: flex;
-//   justify-content: space-between;
-//   align-items: center;
-
-//   @media(max-width: 575px) {
-//     // height: 60px;
-//     width: 100%;
-//   }
-
-//   button {
-//     @media (max-width: 575px) {
-
-//     }
-//   }
-// `;
-
-// const Logo = Styled.img`
-//   height: 50px;
-//   cursor: pointer;
-
-//   @media (max-width: 575px) {
-//     padding-left: 50px;
-//     // height: 40px;
-//   }
-
-//   @media (max-width: 425px) {
-//     padding-left: 25px;
-//     height: 40px;
-//   }
-// `;
-
-// const NavMenu = Styled.div`
-
-//   display: flex;
-//   align-items: center;
-//   justify-content: center;
-
-//   a {
-//     padding: 0 8px;
-//     display: flex;
-//     text-decoration: none;
-
-//     Button:hover {
-//       background-color: #6A994E;
-//       border: 2px solid #6A994E;
-//       color: #ECF0F3;
-//       transition: .3s
-//     }
-
-//     span {
-//       color: #434343;
-//       letter-spacing: .3px;
-//       text-decoration: none;
-//       cursor: pointer;
-//       font-size: 16px;
-//       text-transform: uppercase;
-//     }
-
-//     span:hover {
-//       transition: 0.3s;
-//       color: #6A994E;
-//     }
-//   }
-
-//   @media (max-width: 575px) {
-//     Link {
-//       display: none;
-//     }
-//   }
-
-//   @media (max-width: 425px) {
-//     display: none;
-//   }
-// `;
-
-// const Button = Styled.button`
-// padding: 9px 18px;
-// color: #6A994E;
-// background-color: transparent;
-// border: 2px solid #6A994E;
-// border-radius: 5px;
-// font-size: 16px;
-// text-transform: uppercase;
-// cursor: pointer;
-// box-shadow: 5px 5px 10px rgb(21 230 76 / 26%);
-// font-family: 'Poppins', sans-serif;
-// `;
-
 
 export default Header;
