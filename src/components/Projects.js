a779439bf9d599806dac11eaa827c3436cd6b617
Abdullah8007 <abdullahharishjagrala@gmail.com>
2024-01-09 19:18:23 +0530

    ADD: 147852

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,38 +1,62 @@
-/* eslint-disable jsx-a11y/anchor-is-valid */
+// /* eslint-disable jsx-a11y/anchor-is-valid */
+// import React from 'react';
+// import '../styles/ProjectStyle.css'
+// import { Container } from 'react-bootstrap';
+// import styled from 'styled-components';
+
+// function Projects() {
+//     return (
+//         <Container fluid className='fluid'>
+//             <section className="campus container" id='projects'>
+//                 <h3>Projects</h3>
+//                 <h1>What I've Built</h1>
+//                 <div className="row d-flex align-items-center justify-content-evenly flex-wrap flex-sm-wrap flex-md-wrap gap-3 mt-5">
+//                     <div className="campus-col col-lg-5 col-md-6 col-sm-6">
+//                         <img src="./Images/MovieMetropolis.png" alt="MovieMetropolis" />
+//                         <div className="layer">
+//                             <h3>MovieMetropolis<h5>React JS</h5></h3>
+//                             <a href='https://github.com/Abdullah8007/FULLSTACK-MERN-MOVIES-2023' target='_blank' rel='noreferrer'><Button className='btn'>Code</Button></a>
+//                         </div>
+//                     </div>
+//                     <div className="campus-col col-lg-5 col-md-6 col-sm-6">
+
+//                         <img src="/Images/Eduford.png" alt="Eduford" />
+//                         <div className="layer">
+//                             <h3>Eduford<h5>HTML CSS JS</h5></h3>
+//                             <a href='https://github.com/Abdullah8007/College-Project' target='_blank' rel='noreferrer'><Button className='btn'>Code</Button></a>
+//                         </div>
+//                     </div>
+//                 </div>
+//             </section>
+//         </Container>
+
+//     )
+// };
+
+// const Button = styled.button``;
+
+// export default Projects;
 import React from 'react';
-import '../styles/ProjectStyle.css'
-import { Container } from 'react-bootstrap';
-import styled from 'styled-components';
+import { Col, Container, Row } from 'react-bootstrap';
+import '../styles/ProjectStyle.css';
 
 function Projects() {
     return (
-        <Container fluid className='fluid'>
-            <section className="campus container" id='projects'>
-                <h3>Projects</h3>
-                <h1>What I've Built</h1>
-                <div className="row flex-lg-nowrap flex-sm-wrap">
-                    <div className="campus-col">
-                        <img src="./Images/MovieMetropolis.png" alt="MovieMetropolis" />
-                        <div className="layer">
-                            <h3>MovieMetropolis<h5>React JS</h5></h3>
-                            <a href='https://github.com/Abdullah8007/FULLSTACK-MERN-MOVIES-2023' target='_blank' rel='noreferrer'><Button className='btn'>Code</Button></a>
-                        </div>
-                    </div>
-                    <div className="campus-col">
-                        
-                        <img src="/Images/Eduford.png" alt="Eduford" />
-                        <div className="layer">
-                            <h3>Eduford<h5>HTML CSS JS</h5></h3>
-                            <a href='https://github.com/Abdullah8007/College-Project' target='_blank' rel='noreferrer'><Button className='btn'>Code</Button></a>
-                        </div>
-                    </div>
-                </div>
-            </section>
-        </Container>
-
+        <section className='project-section' id='projects'>
+            <Container className='h-full'>
+                <Row className='row'>
+                    <h3 className='head'>Projects</h3>
+                    <h1 className='title'>What I've Built</h1>
+                    <Col lg={6} className='mb-5 mb-lg-0'>
+                        <img src='/Images/MovieMetropolis.png' alt='MovieMetropolis' className='img-fluid' />
+                    </Col>
+                    <Col lg={6} className='mb-5 mb-lg-0'>
+                        <img src='/Images/Eduford.png' alt='Eduford' className='img-fluid' />
+                    </Col>
+                </Row>
+            </Container>
+        </section>
     )
 };
-
-const Button = styled.button``;
 
 export default Projects;
