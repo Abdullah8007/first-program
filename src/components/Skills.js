d77d5bab6810f1c576ac463351dc3b494e30ac42
Abdullah8007 <abdullahharishjagrala@gmail.com>
2024-05-22 20:06:41 +0530

    ADD:EverLastCommit

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -13,50 +13,56 @@ function Skills() {
                         <h1 className='title'>What I Can Do</h1>
                         <Col md={6} lg={4} className='mb-3 mb-lg-0 mt-3 mt-lg-0 columns'>
                             <div className='skill-menu'>
-                                <img src='/Images/html.webp' alt='About-Img' className='img-fluid' />
+                                <img src='/Images/HTML.svg' alt='Skills-Img' className='img-fluid' />
                                 <h4>HTML5</h4>
                             </div>
                         </Col>
                         <Col md={6} lg={4} className='mb-3 mb-lg-0 mt-3 mt-lg-0 columns'>
                             <div className='skill-menu'>
-                                <img src='/Images/css.png' alt='About-Img' className='img-fluid' />
+                                <img src='/Images/CSS.svg' alt='Skills-Img' className='img-fluid' />
                                 <h4>CSS3</h4>
                             </div>
                         </Col>
                         <Col md={6} lg={4} className='mb-3 mb-lg-0 mt-3 mt-lg-0 columns'>
                             <div className='skill-menu'>
-                                <img src='/Images/javascript.webp' alt='About-Img' className='img-fluid' />
+                                <img src='/Images/JavaScript.svg' alt='Skills-Img' className='img-fluid' />
                                 <h4>JAVASCRIPT</h4>
                             </div>
                         </Col>
                         <Col md={6} lg={4} className='mb-3 mb-lg-0 mt-3 columns'>
                             <div className='skill-menu'>
-                                <img src='/Images/react.png' alt='About-Img' className='img-fluid' />
+                                <img src='/Images/ReactJS.svg' alt='Skills-Img' className='img-fluid' />
                                 <h4>REACT</h4>
                             </div>
                         </Col>
                         <Col md={6} lg={4} className='mb-3 mb-lg-0 mt-3 columns'>
                             <div className='skill-menu'>
-                                <img src='/Images/react-bootstrap.png' alt='About-Img' className='img-fluid' />
+                                <img src='/Images/react-bootstrap.png' alt='Skills-Img' className='img-fluid' />
                                 <h4>REACT BOOTSTRAP</h4>
                             </div>
                         </Col>
                         <Col md={6} lg={4} className='mb-3 mb-lg-0 mt-3 columns'>
                             <div className='skill-menu'>
-                                <img src='/Images/bootstrap.png' alt='About-Img' className='img-fluid' />
+                                <img src='/Images/bootstrap.png' alt='Skills-Img' className='img-fluid' />
                                 <h4>BOOTSTRAP5</h4>
                             </div>
                         </Col>
                         <Col md={6} lg={4} className='mb-3 mb-lg-0 mt-3 columns'>
                             <div className='skill-menu'>
-                                <img src='/Images/github1.webp' alt='About-Img' className='img-fluid' />
+                                <img src='/Images/GitHub.svg' alt='Skills-Img' className='img-fluid' />
                                 <h4>GITHUB</h4>
                             </div>
                         </Col>
                         <Col md={6} lg={4} className='mb-3 mb-lg-0 mt-3 columns'>
                             <div className='skill-menu'>
-                                <img src='/Images/firebase.png' alt='About-Img' className='img-fluid' />
+                                <img src='/Images/Firebase.svg' alt='Skills-Img' className='img-fluid' />
                                 <h4>FIREBASE</h4>
+                            </div>
+                        </Col>
+                        <Col md={6} lg={4} className='mb-3 mb-lg-0 mt-3 columns'>
+                            <div className='skill-menu'>
+                                <img src='/Images/MySQL.svg' alt='Skills-Img' className='img-fluid' />
+                                <h4>MySQL</h4>
                             </div>
                         </Col>
                     </Row>
