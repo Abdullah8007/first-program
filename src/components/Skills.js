2a1ac9a0ef6ae6d8bf17e63b94a90851643c78c6
Abdullah8007 <142715247+Abdullah8007@users.noreply.github.com>
2025-01-02 11:22:34 +0530

    Update Skills.js

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -31,6 +31,12 @@ function Skills() {
                         </Col>
                         <Col md={6} lg={4} className='mb-3 mb-lg-0 mt-3 columns'>
                             <div className='skill-menu'>
+                                <img src='/Images/Python.svg' alt='Skills-Img' className='img-fluid' />
+                                <h4>PYTHON</h4>
+                            </div>
+                        </Col>
+                        <Col md={6} lg={4} className='mb-3 mb-lg-0 mt-3 columns'>
+                            <div className='skill-menu'>
                                 <img src='/Images/ReactJS.svg' alt='Skills-Img' className='img-fluid' />
                                 <h4>REACT</h4>
                             </div>
